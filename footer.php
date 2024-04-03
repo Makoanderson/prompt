3e95e1827f4b3df90a2bd89be9399e5e9e3d4080
makoanderson <yoshidamaryoshidmarad1234@gmail.com>
2024-04-03 16:26:00 +0900

    footerや各コンテンツのlayout調整

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
-<footer class="l-footer c-flex--dirColumn-alignCenter p-footer">
+<footer class="l-footer c-flex--dirColumn-alignCenter p-footer u-w_boxinner u-m_mya">
 
   <article class="p-nav">
     
   </article>
-  <article class="c-logo c-bk_foot">
+  <article class="c-logo c-bk_foot p-f_txtalign p-sup_layout u-txt_color1">
     <figure>
         <p class="p-ft_psec1">改めてプロンプは、<br>美容室の方を対象にLINEを使って電話予約対応やコスト削減など、<br>お店の運用を楽にするお手伝いを行います。</p>
         <p class="p-ft_psec2">機械に対してどんなに苦手意識がある方でも徹底的に向き合います。</p>
--- header.php
+++ header.php
@@ -8,7 +8,7 @@
   <?php wp_head(); ?>
 </head>
 
-<body>
+<body class="c-bk_main">
   <?php /*wp_body_open(); */?>
   <!-- <header class="l-header c-flex--justBetween p-header u-width--100vw">
     <a  class="c-logo__parent" href="<?php //echo esc_url(home_url('/')); ?>">
