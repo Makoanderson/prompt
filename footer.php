343280e1656fcfd9f028d999c849b2efde722b8d
makoanderson <yoshidamaryoshidmarad1234@gmail.com>
2024-04-04 00:50:44 +0900

    cardセクションレイアウト調整

--- footer.php
+++ footer.php
@@ -5,9 +5,9 @@
   </article>
   <article class="c-logo c-bk_foot p-f_txtalign p-sup_layout u-txt_color1">
     <figure>
-        <p class="p-ft_psec1">改めてプロンプは、<br>美容室の方を対象にLINEを使って電話予約対応やコスト削減など、<br>お店の運用を楽にするお手伝いを行います。</p>
-        <p class="p-ft_psec2">機械に対してどんなに苦手意識がある方でも徹底的に向き合います。</p>
-        <p class="p-ft_psec3">一緒にお店の運用を良くしていきましょう。</p>
+        <p class="p-ft_psec1 u-txt_size16 u-m_mb45">改めてプロンプは、<br>美容室の方を対象にLINEを使って電話予約対応やコスト削減など、<br>お店の運用を楽にするお手伝いを行います。</p>
+        <p class="p-ft_psec2 u-txt_size24 u-m_mb64">機械に対してどんなに苦手意識がある方でも徹底的に向き合います。</p>
+        <p class="p-ft_psec3 u-txt_size32 u-m_mb95">一緒にお店の運用を良くしていきましょう。</p>
         <img src="<?php echo esc_url(
                   get_template_directory_uri()
                 ); ?>/img/source/logo.webp"alt="プロンプ" class="img is-mp">
