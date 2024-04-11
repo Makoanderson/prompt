9c1e2fd156ede5640aa6c04ae141dbdbb9cb7d88
makoanderson <yoshidamaryoshidmarad1234@gmail.com>
2024-04-11 12:32:33 +0900

    lp sec2レスポンシブスタイリング

--- header.php
+++ header.php
@@ -10,27 +10,11 @@
 
 <body class="c-bk_main">
   <?php /*wp_body_open(); */?>
-  <!-- <header class="l-header c-flex--justBetween p-header u-width--100vw">
-    <a  class="c-logo__parent" href="<?php //echo esc_url(home_url('/')); ?>">
-      <article class="c-logo">
-        <h1 class="c-flex--dirColumn-alignCenter name">
-          <img src="<?php //echo esc_url(get_template_directory_uri()); ?>/img/pc/Contents01-top.webp" alt="プロンプ" class="img is-mp">
-        </h1>
-        <figure>
-          <p class="p-header_psec1">地方の小さな美容院が<br>お客様の信頼と実績を上げている理由は</p>
-          <img src="<?php //echo esc_url(get_template_directory_uri()); ?>/img/pc/text-top04.webp" alt="LINE" class="">
-          <p class="p-header_psec2">にあり</p>
-          <p class="p-header_psec3">機械が苦手な方も安心サポート</p>
-        </figure>
-        <figure>
-          <img src="<?php //echo esc_url(get_template_directory_uri()); ?>/img/pc/gold-coin.webp" alt="LINE" class="">
-          <img src="<?php //echo esc_url(get_template_directory_uri()); ?>/img/pc/CTA.webp" alt="LINE" class="">
-        </figure>
-      </article>
-    </a>
-    <article class="p-nav">
-      
-
-    </article>
-
-  </header> -->
+  <header class="l-header c-flex--justBetween p-header u-width--100vw u-disp_noneblock">
+    
+    <h1 class="c-bk_spheader u-txt_color1 u-txt_size10b u-m_ml16 l-header_logo u-m_my10">
+      <p class="u-m_mb7">あなたの街のLINEコーディネーター</p>
+      <img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/source/logo.webp" alt="プロンプロゴ" class="l-header_logoimg">
+    </h1>
+        
+  </header>
