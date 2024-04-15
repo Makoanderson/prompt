70687dc2a6b206df2c962fa430bfb0f1fe625b91
makoanderson <yoshidamaryoshidmarad1234@gmail.com>
2024-04-15 17:08:42 +0900

    プランセクション作成

--- footer.php
+++ footer.php
@@ -3,11 +3,11 @@
   <article class="p-nav">
     
   </article>
-  <article class="c-logo c-bk_foot p-f_txtalign p-sup_layout u-txt_color1">
-    <figure class="u-w_420tab341 u-m_mya">
-        <p class="p-ft_psec1 u-txt_size16 u-m_mb45">改めてプロンプは、<br>美容室の方を対象にLINEを使って電話予約対応やコスト削減など、<br>お店の運用を楽にするお手伝いを行います。</p>
+  <article class="c-logo c-bk_foot p-sup_layout u-txt_color1">
+    <figure class="u-w_640tab341 u-m_mya">
+        <p class="p-ft_psec1 u-txt_size16 u-m_mb45 u-w_ft-disc">改めてプロンプは、<br>美容室の方を対象にLINEを使って電話予約対応やコスト削減など、お店の運用を楽にするお手伝いを行います。</p>
         <p class="p-ft_psec2 u-txt_size24 u-m_mb64">機械に対してどんなに苦手意識がある方でも徹底的に向き合います。</p>
-        <p class="p-ft_psec3 u-txt_size32 u-m_mb95">一緒にお店の運用を良くしていきましょう。</p>
+        <p class="p-ft_psec3 u-txt_size32 u-m_mb95 u-txt_shadow">一緒にお店の運用を良くしていきましょう。</p>
         <img src="<?php echo esc_url(
                   get_template_directory_uri()
                 ); ?>/img/source/logo.webp"alt="プロンプ" class="u-m_mx20p">
