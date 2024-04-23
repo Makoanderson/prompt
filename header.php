07defb8864fbe2d52b50c4cf9a0f536fb39555b9
makoanderson <yoshidamaryoshidmarad1234@gmail.com>
2024-04-24 00:04:31 +0900

    mv修正、カード部分修正

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="promptのlpサイト">
+  <script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
   <?php wp_head(); ?>
 </head>
 
